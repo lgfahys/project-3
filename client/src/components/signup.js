69b8678bf6ba4cb5f559b1ffa05c681e1061b613
yevheniia01 <jene4ka1818@gmail.com>
2019-06-28 14:28:06 -0400

    react router added

---
+++ client/src/components/signup.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./media.css"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
+
+
+class Sign extends Component {
+    render() {
+      return (
+        <div className="SignUp">
+            <h1>Hello</h1>
+            </div>
+      )
+    }
+}
+
+export default SignUp;
