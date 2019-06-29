58211b067b6cdc18c055955bf22c330f1efcc772
csvernon <csvernon@ncsu.edu>
2019-06-29 13:53:53 -0400

    lindsey made me overwrite Jenna's login page

--- client/src/components/pages/SignUp/index.js
+++ client/src/components/pages/SignUp/index.js
@@ -9,56 +9,82 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Image from 'react-bootstrap/Image'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+import { MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon, MDBModalFooter } from 'mdbreact';
 
 
 class SignUp extends Component {
-    render() {
-      return (
-        <div className ="App">
+  render() {
+    return (
+      <div className="App">
         <Nav variant="pills" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Image className="navImage" src="../../../assets/images/mail.png"/>
-                </Nav.Item>
-                <Nav.Item className="firstNavItem">
-                    <Nav.Link eventKey="link-1">LOGIN</Nav.Link>
-                </Nav.Item>
-                <Nav.Item className="secondNavItem">
-                    <Nav.Link eventKey="link-2">SIGN UP</Nav.Link>
-                </Nav.Item>
-            </Nav>
-            <Container>
-              <Row className="cardRow">
-                <Col sm={5} md={9} lg={9} mx-auto className="cardColmn">
-                  <Card className="card-signin my-S">
-                    <Card.Body>
-                      <h5 className="card-title text-center">Sign In</h5>
-                      <form class="form-signin">
-                      <div class="form-label-group">
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus/>
-                <label for="inputEmail">Email address</label>
-              </div>
-              <div class="form-label-group">
-                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
-                <label for="inputPassword">Password</label>
-              </div>
-              <div class="custom-control custom-checkbox mb-3">
-                <input type="checkbox" class="custom-control-input" id="customCheck1"/>
-                <label class="custom-control-label" for="customCheck1">Remember password</label>
-              </div>
-              <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-              <hr class="my-4"/>
-              <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
-              <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
-            </form>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
+          <Nav.Item>
+            <Image className="navImage" src="../../../assets/images/mail.png" />
+          </Nav.Item>
+          <Nav.Item className="topLoginBtn">
+            <Nav.Link eventKey="link-1">LOGIN</Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="topSignUpBtn">
+            <Nav.Link eventKey="link-2">SIGN UP</Nav.Link>
+          </Nav.Item>
+          </Nav>
+          <MDBContainer>
+            <MDBRow>
+              <MDBCol md="6">
+                <MDBCard>
+                  <MDBCardBody className="mx-4">
+                    <div className="text-center">
+                      <h3 className="dark-grey-text mb-5">
+                        <strong>Sign in</strong>
+                      </h3>
+                    </div>
+                    <MDBInput
+                      label="Your email"
+                      group
+                      type="email"
+                      validate
+                      error="wrong"
+                      success="right"
+                    />
+                    <MDBInput
+                      label="Your password"
+                      group
+                      type="password"
+                      validate
+                      containerClass="mb-0"
+                    />
+                    <p className="font-small blue-text d-flex justify-content-end pb-3">
+                      Forgot
+                <a href="#!" className="blue-text ml-1">
 
-            </Container>
-            </div>
-      )
-    }
-}
+                        Password?
+                </a>
+                    </p>
+                    <div className="text-center mb-3">
+                      <MDBBtn
+                        type="button"
+                        gradient="blue"
+                        rounded
+                        className="btn-block z-depth-1a"
+                      >
+                        Sign in
+                </MDBBtn>
+                    </div>                    
+                  </MDBCardBody>
+                  <MDBModalFooter className="mx-5 pt-3 mb-1">
+                    <p className="font-small grey-text d-flex justify-content-end">
+                      Not a member?
+                <a href="#!" className="blue-text ml-1">
 
+                        Sign Up
+                </a>
+                    </p>
+                  </MDBModalFooter>
+                </MDBCard>
+              </MDBCol>
+            </MDBRow>
+          </MDBContainer>
+        </div>
+        )
+      }
+  }
 export default SignUp;
