02c4ec48be9068258bf8528254a8b5942e59cc0f
Emery I <ihegaranyau@gmail.com>
2019-07-01 20:42:07 -0400

    created files

---
+++ client/src/components/pages/Profile/index.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import "./style.css";
+import "./media2.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav'
+import Row from 'react-bootstrap/Row'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Image from 'react-bootstrap/Image'
+import Col from 'react-bootstrap/Col'
+const signUpPage = () => {
+  return (
+    <Container fluid>
+      <Row className="signUpRow"> 
+      <Col xs={12} md={2} lg={4}></Col>
+        <Col xs={12} md={8} lg={4}>
+          <MDBCard>
+            <MDBCardBody>
+              <form>
+                <p className="h4 text-center py-4">Sign up</p>
+                <div className="grey-text">
+                  <MDBInput
+                    label="First Name"
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Email"
+                    icon="envelope"
+                    group
+                    type="email"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Phone"
+                    icon="phone"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Gender"
+                    icon="user"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                  <MDBInput
+                    label="Your password"
+                    icon="lock"
+                    group
+                    type="password"
+                    validate
+                  />
+                  <MDBInput
+                    label="Confirm Password"
+                    icon="exclamation-triangle"
+                    group
+                    type="text"
+                    validate
+                    error="wrong"
+                    success="right"
+                  />
+                </div>
+                <div className="text-center py-4 mt-3">
+                  <MDBBtn  gradient="blue" type="submit">
+                    Sign Up
+                  </MDBBtn>
+                </div>
+              </form>
+            </MDBCardBody>
+          </MDBCard>
+        </Col>
+        <Col xs={12} md={2} lg={4}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default signUpPage;
