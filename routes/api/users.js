8b0371656bd242056d3976219a67378d4b3d1eb6
Elvin <elveng@gmail.com>
2019-07-01 20:06:52 -0400

    added mongo connection and schema

---
+++ routes/api/users.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/userController");
+
+// Matches with "/api/books"
+// router
+//     .route("/")
+//     .get(booksController.findAll)
+//     .post(booksController.create);
+
+// // Matches with "/api/books/:id"
+// router
+//     .route("/:id")
+//     .get(booksController.findById)
+//     .put(booksController.update)
+//     .delete(booksController.remove);
+
+// Matched with "/api/books/*"
+// router
+//     .route("*")
+//     .get();
+
+module.exports = router;
