5c99ef4ba7ca85459b7ba156974c932ce93659d7
csvernon <csvernon@ncsu.edu>
2019-07-03 20:33:33 -0400

    change field descriptions

--- client/src/components/pages/Home/components/PplNearYou.js
+++ client/src/components/pages/Home/components/PplNearYou.js
@@ -8,8 +8,8 @@ function PplNearYou (){
         <Row className='userRow'>
             <Col sm="1" md="1" lg="1"><img className="user" alt="user-icon" src="../../../assets/images/user.png "/> </Col>
             <Col className='col' sm="2" md="2" lg="2">Name</Col>
-            <Col sm="3" md="3" lg="3"><button className="btn btn-outline-default">Primary<i className="fas far fa-plus-square pl-1"></i></button></Col>
-            <Col sm="3" md="3" lg="3"><button className="btn btn-default">Primary<i className="fas fas fa-user pl-1"></i></button></Col>
+            <Col sm="3" md="3" lg="3"><button className="btn btn-outline-default">Chat<i className="fas far fa-plus-square pl-1"></i></button></Col>
+            <Col sm="3" md="3" lg="3"><button className="btn btn-default">Profile<i className="fas fas fa-user pl-1"></i></button></Col>
         </Row>   
     )
 }
