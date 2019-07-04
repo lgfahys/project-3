261d4009ca5b6e583b29b846ce5622ffaed380f6
Emery I <ihegaranyau@gmail.com>
2019-07-03 21:10:23 -0400

    profile edit

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -33,7 +33,7 @@ class Profile extends React.Component {
                 <MDBCard>
                   <MDBCardBody>
                     <form>
-                      <p className="h4 text-center py-4">Sign up</p>
+                      <p className="h4 text-center py-4">Marie Johnson</p>
                       <div className="grey-text">
                         <MDBInput
                           label="First Name"
@@ -110,60 +110,14 @@ class Profile extends React.Component {
                 <MDBCard>
                   <MDBCardBody>
                     <form>
-                      <p className="h4 text-center py-4">Sign up</p>
+                      <p className="h4 text-center py-4">About Me</p>
                       <div className="grey-text">
-                        <MDBInput
-                          label="First Name"
-                          icon="user"
-                          group
-                          type="text"
-                          validate
-                          error="wrong"
-                          success="right"
-                        />
-                        <MDBInput
-                          label="Email"
-                          icon="envelope"
-                          group
-                          type="email"
-                          validate
-                          error="wrong"
-                          success="right"
-                        />
-                        <MDBInput
-                          label="Phone"
-                          icon="phone"
-                          group
-                          type="text"
-                          validate
-                          error="wrong"
-                          success="right"
-                        />
-                        <MDBInput
-                          label="Gender"
-                          icon="user"
-                          group
-                          type="text"
-                          validate
-                          error="wrong"
-                          success="right"
-                        />
-                        <MDBInput
-                          label="Your password"
-                          icon="lock"
-                          group
-                          type="password"
-                          validate
-                        />
-                        <MDBInput
-                          label="Confirm Password"
-                          icon="exclamation-triangle"
-                          group
-                          type="text"
-                          validate
-                          error="wrong"
-                          success="right"
-                        />
+                      <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Maxime quae, dolores dicta. Blanditiis rem amet repellat,
+                  dolores nihil quae in mollitia asperiores ut rerum
+                  repellendus, voluptatum eum, officia laudantium quaerat?
+                </p>
                       </div>
                       <div className="text-center py-4 mt-3">
                         <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
@@ -175,7 +129,7 @@ class Profile extends React.Component {
                 </MDBCard>
               </Col>
               <Col xs={12} md={2} lg={4}></Col>
-            </Row>y
+            </Row>
     </Container>
         </div>
       </ReactCardFlip>
