1cb8883035ea321618de91a2dc80cf11481deab6
csvernon <csvernon@ncsu.edu>
2019-07-06 12:07:17 -0400

    added location logging to index.html, should be able to push to mongo

--- client/src/components/pages/Chat/index.js
+++ client/src/components/pages/Chat/index.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import { MDBCard, MDBCardBody, MDBRow, MDBCol, MDBListGroup, MDBListGroupItem, MDBBadge, MDBIcon, MDBBtn, MDBScrollbar } from "mdbreact";
+// import { MDBCard, MDBCardBody, MDBRow, MDBCol, MDBListGroup, MDBListGroupItem, MDBBadge, MDBIcon, MDBBtn, MDBScrollbar } from "mdbreact";
 //import "./ChatPage.css";
 
 import ChatUser from "./components/ChatUser"
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 import ChatBody from "./components/ChatBody"
 import ChatInput from "./components/ChatInput"
 
--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactCardFlip from 'react-card-flip';
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import {MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+// import { MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdbreact';
 import "./style.css";
 import "./media2.css";
 import Container from 'react-bootstrap/Container';
