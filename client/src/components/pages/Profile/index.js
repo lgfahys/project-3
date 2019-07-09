cd30345b50f8489d23844c9636606b633300f12e
Emery I <ihegaranyau@gmail.com>
2019-07-08 21:25:34 -0400

    additional changes to profile page

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -68,7 +68,7 @@ class Profile extends React.Component {
                       <p className="h1 text-center py-4">Marie Johnson</p>
                       </div>
                       <hr size="10"></hr>
-                      <div className="grey-text">
+                      <div className= "font-weight-bold blue-text">
                       <p className="h4 text-center py-4">Web Developer</p>
                       </div>
                       <hr size="10"></hr>
@@ -101,9 +101,9 @@ class Profile extends React.Component {
                       <p className="h4 text-center py-4">About Me</p>
                       <div className="grey-text">
                       <p>
-                      Hi my name is Marie! I am new to the area but love meeting new
-                      people. My favorite place to meet new people is in the downtown area. I can't wait to meet new
-                      friends in the city to start chatting with!
+                      Hi my name is Marie! I am new to the area but love to meet new
+                      people. My favorite place to socialize is in the downtown area. I can't wait to meet new
+                      friends in the city to start "chatting" with!
                        
                 </p>
                       </div>
