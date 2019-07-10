448c93e880726b00d0163ef2b8f6c814b03fa682
Emery I <ihegaranyau@gmail.com>
2019-07-10 00:51:30 -0400

    render profile page with dummy data

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactCardFlip from 'react-card-flip';
-import {MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
+import { MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
 // import { MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdbreact';
 import "./style.css";
 import "./media2.css";
@@ -9,21 +9,52 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import profile from '../assets/marie.jpeg';
 
+// const API = '';
+// const DEFAULT_QUERY = 'redux';
+const data = {
+  'name': 'Marie Johnson',
+  'job': 'Web Developer',
+  'gender': 'Female',
+  'birthdate': 'January 1, 1989',
+  'image': 'profile',
+  'bio': "Hi my name is Marie! I am new to the area but love to meet new people. My favorite place to socialize is in the downtown area. I can't wait to meet new friends in the city to start chatting with!",
+  'link': 'linkedIn.com/marie'
+};
+
 class Profile extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      isFlipped: false
+      profiles: [],
+      isFlipped: false,
+      // isLoading: false,
+      // error: null,
     };
     this.handleClick = this.handleClick.bind(this);
   }
-
+  //Handles set of flip
   handleClick(e) {
     e.preventDefault();
     this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
   }
+  //Render data from backend
+  componentDidMount() {
+    this.setState({ profiles: data })
+    //   this.setState({ isLoading: true });
 
+    //   axios.get(API + DEFAULT_QUERY)
+    //     .then(result => this.setState({
+    //       profiles: result.data,
+    //       isLoading: false
+    //     }))
+    //     .catch(error => this.setState({
+    //       error,
+    //       isLoading: false
+    //     }));
+  }
+  //Render data to frontend
   render() {
+    const { profiles } = this.state;
     return (
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
         <div key="front">
@@ -34,48 +65,64 @@ class Profile extends Component {
                 <MDBCard>
                   <MDBCardBody>
                     <form>
-                     <div>
-                     <Row className="userRow">
-        <Col sm="2" md="2" lg="2">
-        {/* <img className="arrowImg" src="../../../assets/images/left-arrow.png" /> */}
-        </Col>
-        <Col sm="2" md="2" lg="2">
+                      <div>
+                        <Row className="userRow">
+                          <Col sm="2" md="2" lg="2">
+                            {/* <img className="arrowImg" src="../../../assets/images/left-arrow.png" /> */}
+                          </Col>
+                          <Col sm="2" md="2" lg="2">
 
-        </Col>
-        <Col sm="2" md="2" lg="2">
-        <img src={profile} alt="profile"
-                     style={{
-                      // alignSelf: 'center',
-                      height: 150,
-                      width: 150,
-                      borderWidth: 1,
-                      borderRadius: 75,
-                      justifyContent: 'center',
-                     alignItems: 'center',
-                    }}
-                    resizeMode="stretch"
-                     />
-        </Col>
-        <Col sm="2" md="2" lg="2">
-            
-            </Col>
-        <Col sm="3" md="3" lg="3">
-        {/* <button class="btn btn-danger">Primary<i class="fas  pl-1"></i></button> */}
-        </Col>
-        </Row>
-                     </div>
+                          </Col>
+                          <Col sm="2" md="2" lg="2">
+                            <img src={profile} alt="profile"
+                              style={{
+                                // alignSelf: 'center',
+                                height: 150,
+                                width: 150,
+                                borderWidth: 1,
+                                borderRadius: 75,
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                              }}
+                              resizeMode="stretch"
+                            />
+                          </Col>
+                          <Col sm="2" md="2" lg="2">
+
+                          </Col>
+                          <Col sm="3" md="3" lg="3">
+                            {/* <button class="btn btn-danger">Primary<i class="fas  pl-1"></i></button> */}
+                          </Col>
+                        </Row>
+                      </div>
                       <hr size="10"></hr>
                       <div className="grey-text">
-                      <p className="h1 text-center py-4">Marie Johnson</p>
+                        <p className="h1 text-center py-4">{profiles.name}</p>
                       </div>
                       <hr size="10"></hr>
+                      <div className="font-weight-bold blue-text">
+                        <p className="h5 text-center py-4">{profiles.job}</p>
+                      </div>
+                      <hr size="10"></hr>
+                      <div className="font-weight-bold blue-text">
+                        <p className="h6 text-center py-4">{profiles.gender}</p>
+                      </div>
+                      <hr size="10"></hr>
+                      <div className="font-weight-bold blue-text">
+                        <p className="h6 text-center py-4">{profiles.birthdate}</p>
+                      </div>
+                      <hr size="10"></hr>
+                      <div className="font-weight-bold blue-text">
+                        <p className="h6 text-center py-4">{profiles.link}</p>
+                      </div>
+                      {/* <hr size="10"></hr>
                       <div className= "font-weight-bold blue-text">
-                      <p className="h4 text-center py-4">Web Developer</p>
-                      </div>
-                      <hr size="10"></hr>
+                      <p className="h4 text-center py-4">{profiles.gender}</p>
+                      </div> */}
+                      {/* <hr size="10"></hr>
                       <div className="grey-text">
                     
-                      </div>
+                      </div> */}
                       <hr size="10"></hr>
                       <div className="text-center py-4 mt-3">
                         <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
@@ -101,12 +148,7 @@ class Profile extends Component {
                     <form>
                       <p className="h4 text-center py-4">About Me</p>
                       <div className="grey-text">
-                      <p>
-                      Hi my name is Marie! I am new to the area but love to meet new
-                      people. My favorite place to socialize is in the downtown area. I can't wait to meet new
-                      friends in the city to start "chatting" with!
-                       
-                </p>
+                        <p>{profiles.bio}</p>
                       </div>
                       <div className="text-center py-4 mt-3">
                         <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
@@ -119,7 +161,7 @@ class Profile extends Component {
               </Col>
               <Col xs={12} md={2} lg={4}></Col>
             </Row>
-    </Container>
+          </Container>
         </div>
       </ReactCardFlip>
     )
