de20f19ed04f6fee9b2ead33cc1fcb6c16af0c41
Elvin <elveng@gmail.com>
2019-07-15 22:47:54 -0400

    updated profile page styling

--- client/src/components/pages/Profile/index.js
+++ client/src/components/pages/Profile/index.js
@@ -1,37 +1,34 @@
-import React, { Component } from 'react';
-import ReactCardFlip from 'react-card-flip';
-import { MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
-// import { MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdbreact';
+import React, { Component } from "react";
+import ReactCardFlip from "react-card-flip";
+import { MDBBtn, MDBCard, MDBCardBody } from "mdbreact";
 import "./style.css";
 import "./media2.css";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+
+import { Row, Col } from "react-bootstrap"
+import API from "../../../utils/API";
 
 import profile from '../assets/marie.jpeg';
-
-import API from "../../../utils/API";
 
 class Profile extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       profiles: "",
-      isFlipped: false,
-      // isLoading: false,
-      // error: null,
+      isFlipped: false
     };
-    this.handleClick = this.handleClick.bind(this);
   }
+  
   //Handles set of flip
-  handleClick(e) {
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped }));
-  }
-  //Render data from backend
-  componentDidMount() {
-    // this.setState({ profiles: data })
+  handleClick = (event) => {
+    event.preventDefault();
 
+    this.setState((prevState) => ({
+      isFlipped: !prevState.isFlipped
+    }));
+  }
+
+  componentDidMount() {
     API
       .getProfileByUser(this.props.location.search.substr(1))
       .then(res => {
@@ -53,72 +50,68 @@ class Profile extends Component {
     return (
     <div className="profile-page">
       <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="vertical">
-        <div key="front">
-          <Container fluid>
-            <Row className="signUpRow">
-              {/* <Col xs={12} md={2} lg={4}></Col> */}
-              {/* <Col className="front-page" xs={12} md={8} lg={4}> */}
-              <Col className="front-page">
-                <MDBCard className="front-body">
-                  <MDBCardBody >
-                      <div>
-                        <Row className="userRow">
-                          <Col className="profile-row1">
-                            <img  className="profile-image" src={profile} alt="profile"/>
-                          </Col>
-                        </Row>
-                      </div>
-                      <hr size="5"></hr>
-                      <div className="grey-text">
-                        <p className="h1 text-center py-4">{profiles.name}</p>
-                      </div>
-                      <hr size="5"></hr>
-                      <div className="font-weight-bold blue-text">
-                        <p className="h6 text-center py-4">{profiles.gender}</p>
-                      </div>
-                      <hr size="5"></hr>
-                      <div className="font-weight-bold blue-text">
-                        <p className="h6 text-center py-4">{profiles.age}</p>
-                      </div>
-                      <hr size="5"></hr>
-                      <div className="text-center py-4 mt-3">
-                        <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
-                          More Info
-                  </MDBBtn>
-                      </div>
-                  </MDBCardBody>
-                </MDBCard>
-                </Col>
-              {/* </Col> */}
-              {/* <Col xs={12} md={2} lg={4}></Col> */}
-            </Row>y
-    </Container>
+        <div className="container front-page" key="front">
+          <Row className="signUpRow">
+            <Col>
+              <MDBCard>
+                <MDBCardBody >
+                  <Row className="userRow">
+                    <Col className="profile-row1">
+                      <img  className="profile-image" src={profile} alt="profile"/>
+                    </Col>
+                  </Row>
+                  <hr />
+                  <div className="grey-text">
+                    <p className="h1 text-center py-2">{profiles.name}</p>
+                  </div>
+                  <hr />
+                  <p className="h4 text-center py-3">About Me</p>
+                  <div className="grey-text profile-bio">
+                    <p>{profiles.bio}</p>
+                  </div>
+                  <div className="text-center py-2 mt-3">
+                    <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
+                      More Info
+                    </MDBBtn>
+                  </div>
+                </MDBCardBody>
+              </MDBCard>
+              </Col>
+          </Row>
         </div>
 
-        <div key="back">
-          <Container fluid>
+        <div className="container back-page" key="back">
             <Row className="signUpRow">
-              <Col xs={12} md={2} lg={4}></Col>
-              <Col xs={12} md={8} lg={4}>
+              <Col>
                 <MDBCard>
                   <MDBCardBody>
-                    <form>
-                      <p className="h4 text-center py-4">About Me</p>
-                      <div className="grey-text">
-                        <p>{profiles.bio}</p>
-                      </div>
-                      <div className="text-center py-4 mt-3">
-                        <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
-                          Back To Profile
-                  </MDBBtn>
-                      </div>
-                    </form>
+                    <Row className="userRow">
+                      <Col className="profile-row1">
+                        <img  className="profile-image" src={profile} alt="profile"/>
+                      </Col>
+                    </Row>
+                    <hr />
+                    <div className="grey-text">
+                      <p className="h1 text-center py-2">{profiles.name}</p>
+                    </div>
+                    <hr />
+                    <div className="font-weight-bold blue-text">
+                      <p className="h5 text-center py-2">{profiles.gender}</p>
+                    </div>
+                    <hr />
+                    <div className="font-weight-bold blue-text">
+                      <p className="h5 text-center py-2">{profiles.age}</p>
+                    </div>
+                    <hr />
+                    <div className="text-center py-4 mt-3">
+                      <MDBBtn gradient="blue" type="submit" onClick={this.handleClick}>
+                        Back To Profile
+                      </MDBBtn>
+                    </div>
                   </MDBCardBody>
                 </MDBCard>
               </Col>
-              <Col xs={12} md={2} lg={4}></Col>
             </Row>
-          </Container>
         </div>
       </ReactCardFlip>
     </div>
