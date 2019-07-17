9827454a7548ed61befc49e0bc10bad8c79a9121
Elvin <elveng@gmail.com>
2019-07-16 22:59:41 -0400

    updated chat page styling

--- client/src/components/pages/Chat/index.js
+++ client/src/components/pages/Chat/index.js
@@ -72,19 +72,16 @@ class ChatPage extends Component {
             //     <ChatBody messages={this.state.messages}/>
             //     <ChatInput changemessage={this.changeMessage} message={this.state.message} sendmessage={this.sendMessage} />
             // </Container>
-            <div className="container">
-            <div className="row chat-window col-xs-4 col-md-6" id="chat_window_1" style={{marginLeft: "10px"}}>
-                <div className="col-xs-12 col-md-12">
+            <div className="container-chat container">
+            <div className="row chat-window col-xs-4 col-md-6">
+                <div className="chat-scope col-xs-12 col-md-12">
                     <div className="panel panel-default">
                         <div className="panel-heading top-bar">
                             <div className="col-md-8 col-xs-8">
-                                <h3 className="panel-title"><span className="glyphicon glyphicon-comment"></span> Chat - Miguel</h3>
-                            </div>
-                            <div className="col-md-4 col-xs-4" style={{textAlign: "right"}}>
-                                <a href="#"><span id="minim_chat_window" className="glyphicon glyphicon-minus icon_minim"></span></a>
-                                <a href="#"><span className="glyphicon glyphicon-remove icon_close" data-id="chat_window_1"></span></a>
+                                <h3 className="panel-title">Chatter - Miguel</h3>
                             </div>
                         </div>
+                            
                         <div className="panel-body msg_container_base">
                             <div className="row msg_container base_sent">
                                 <div className="col-md-10 col-xs-10">
@@ -158,12 +155,65 @@ class ChatPage extends Component {
                                     <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
                                 </div>
                             </div>
+
+                            <div className="row msg_container base_sent">
+                                <div className="col-md-10 col-xs-10 ">
+                                    <div className="messages msg_sent">
+                                        <p>that mongodb thing looks good, huh?
+                                        tiny master db, and huge document store</p>
+                                        <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 col-xs-2 avatar">
+                                    <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
+                                </div>
+                            </div>
+                            <div className="row msg_container base_sent">
+                                <div className="col-md-10 col-xs-10 ">
+                                    <div className="messages msg_sent">
+                                        <p>that mongodb thing looks good, huh?
+                                        tiny master db, and huge document store</p>
+                                        <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 col-xs-2 avatar">
+                                    <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
+                                </div>
+                            </div>
+                            <div className="row msg_container base_sent">
+                                <div className="col-md-10 col-xs-10 ">
+                                    <div className="messages msg_sent">
+                                        <p>that mongodb thing looks good, huh?
+                                        tiny master db, and huge document store</p>
+                                        <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 col-xs-2 avatar">
+                                    <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
+                                </div>
+                            </div>
+                            <div className="row msg_container base_sent">
+                                <div className="col-md-10 col-xs-10 ">
+                                    <div className="messages msg_sent">
+                                        <p>that mongodb thing looks good, huh?
+                                        tiny master db, and huge document store</p>
+                                        <time datetime="2009-11-13T20:00">Timothy • 51 min</time>
+                                    </div>
+                                </div>
+                                <div className="col-md-2 col-xs-2 avatar">
+                                    <img src="http://www.bitrebels.com/wp-content/uploads/2011/02/Original-Facebook-Geek-Profile-Avatar-1.jpg" className=" img-responsive " />
+                                </div>
+                            </div>
+
+
                         </div>
+
+
                         <div className="panel-footer">
                             <div className="input-group">
                                 <input id="btn-input" type="text" className="form-control input-sm chat_input" placeholder="Write your message here..." />
                                 <span className="input-group-btn">
-                                <button className="btn btn-primary btn-sm" id="btn-chat">Send</button>
+                                <button className="btn btn-dark gray btn-sm" id="btn-chat">Send</button>
                                 </span>
                             </div>
                         </div>
@@ -171,19 +221,7 @@ class ChatPage extends Component {
                 </div>
             </div>
             
-            <div className="btn-group dropup">
-                <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    <span className="glyphicon glyphicon-cog"></span>
-                    <span className="sr-only">Toggle Dropdown</span>
-                </button>
-                <ul className="dropdown-menu" role="menu">
-                    <li><a href="#" id="new_chat"><span className="glyphicon glyphicon-plus"></span> Novo</a></li>
-                    <li><a href="#"><span className="glyphicon glyphicon-list"></span> Ver outras</a></li>
-                    <li><a href="#"><span className="glyphicon glyphicon-remove"></span> Fechar Tudo</a></li>
-                    <li className="divider"></li>
-                    <li><a href="#"><span className="glyphicon glyphicon-eye-close"></span> Invisivel</a></li>
-                </ul>
-            </div>
+
         </div>
 
 
